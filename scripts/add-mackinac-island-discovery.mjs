ec80120bec3ccc1d9d180e41d3b65318f13f8527
Chris Izworski <46615230+izworskic@users.noreply.github.com>
2026-09-20 15:12:34 -0400

    Add Mackinac mobility and bike pages to discovery

--- scripts/add-mackinac-island-discovery.mjs
+++ scripts/add-mackinac-island-discovery.mjs
@@ -12,7 +12,9 @@ if(fs.existsSync(sitemap)){
     ['https://chrisizworski.com/mackinac-island/from-detroit/','weekly','0.80'],
     ['https://chrisizworski.com/mackinac-island/from-chicago/','weekly','0.80'],
     ['https://chrisizworski.com/mackinac-island/from-traverse-city/','weekly','0.80'],
-    ['https://chrisizworski.com/mackinac-island/from-grand-rapids/','weekly','0.80']
+    ['https://chrisizworski.com/mackinac-island/from-grand-rapids/','weekly','0.80'],
+    ['https://chrisizworski.com/mackinac-island/limited-walking/','weekly','0.84'],
+    ['https://chrisizworski.com/mackinac-island/bike-day/','weekly','0.84']
   ];
   for(const [loc,freq,priority] of urls){
     if(xml.includes('<loc>'+loc+'</loc>'))continue;
