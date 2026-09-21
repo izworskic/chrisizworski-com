4e1b1a4fd6482df324883300c3abd22dffde5ded
Chris Izworski <46615230+izworskic@users.noreply.github.com>
2026-09-21 09:05:17 -0400

    Fix Detroit Outdoors dispatcher syntax

--- api/fall-color.js
+++ api/fall-color.js
@@ -17,7 +17,8 @@ const handlers = {
   sitemap: require("../lib/fall-color/routes/sitemap.js"),
   cron: require("../lib/fall-color/routes/cron.js"),
   "old-sow-live": require("../lib/old-sow/route.js"),
-  "mackinac-island": require("../lib/mackinac-island/route.js"),\n  "detroit-outdoors": require("../lib/detroit-outdoors/route.js"),
+  "mackinac-island": require("../lib/mackinac-island/route.js"),
+  "detroit-outdoors": require("../lib/detroit-outdoors/route.js"),
 };
 
 module.exports = async (req, res) => {
