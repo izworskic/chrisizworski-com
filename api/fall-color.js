79d8b38ee632203187a4fa9d8b4f9adc1c1c7f21
Chris Izworski <46615230+izworskic@users.noreply.github.com>
2026-09-21 08:57:50 -0400

    Wire Detroit Outdoors through existing API dispatcher

--- api/fall-color.js
+++ api/fall-color.js
@@ -17,7 +17,7 @@ const handlers = {
   sitemap: require("../lib/fall-color/routes/sitemap.js"),
   cron: require("../lib/fall-color/routes/cron.js"),
   "old-sow-live": require("../lib/old-sow/route.js"),
-  "mackinac-island": require("../lib/mackinac-island/route.js"),
+  "mackinac-island": require("../lib/mackinac-island/route.js"),\n  "detroit-outdoors": require("../lib/detroit-outdoors/route.js"),
 };
 
 module.exports = async (req, res) => {
