1f71ac2c5d51e9468d25d24f84f2d5bba3484def
Chris Izworski <46615230+izworskic@users.noreply.github.com>
2026-09-24 17:58:41 -0400

    Use future-aware Gatlinburg decision health

--- api/gatlinburg-winter.js
+++ api/gatlinburg-winter.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const handler = require("../lib/gatlinburg-winter/route-v9.js");
+const handler = require("../lib/gatlinburg-winter/route-v10.js");
 
 function gatlinburgWinter(req, res) {
   res.setHeader("X-Robots-Tag", "noindex");
