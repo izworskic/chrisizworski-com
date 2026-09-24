ffc4caa03eb9811f97ec02d1e0b0bfc1ff184c18
Chris Izworski <46615230+izworskic@users.noreply.github.com>
2026-09-24 11:16:45 -0400

    Honor API noindex contract for Blue Ridge planner

--- api/blue-ridge-parkway.js
+++ api/blue-ridge-parkway.js
@@ -1,2 +1,8 @@
 "use strict";
-module.exports=require("../lib/blue-ridge-parkway/engine.js");
+
+const handler=require("../lib/blue-ridge-parkway/engine.js");
+
+module.exports=async function blueRidgeParkway(req,res){
+  res.setHeader("X-Robots-Tag","noindex, nofollow");
+  return handler(req,res);
+};
